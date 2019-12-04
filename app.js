e9997d2d36502724f6e66ac1ca1dfaa57dcf1f67
İsa <isatoltar@gmail.com>
2019-12-04 15:43:44 +0300

    login and signup

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, './public/')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', usersRouter);
 app.use('/', matchRouter);
 
 // catch 404 and forward to error handler
