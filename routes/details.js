049534f52cffa90a09ddcd877b146a42d77255d0
İsa <isatoltar@gmail.com>
2019-12-12 22:50:51 +0300

    chunk comments deleted

--- routes/details.js
+++ routes/details.js
@@ -5,20 +5,4 @@ router.get('/main', function(req, res) {
   res.render('singlepage');
 });
 
-// router.get('/crime', function(req, res) {
-//   res.render('crime');
-// });
-
-// router.get('/realestate', function(req, res) {
-//   res.render('realestate');
-// });
-
-// router.get('/schools', function(req, res) {
-//   res.render('schools');
-// });
-
-// router.get('/citydetails', function(req, res) {
-//   res.render('citydetails');
-// });
-
 module.exports = router;
