d76f0154c76cdc322c9db4dfb83f87d6e90d5cc3
İsa <isatoltar@gmail.com>
2019-12-12 22:40:02 +0300

    details page structure pivoted to tab view

--- routes/details.js
+++ routes/details.js
@@ -1,24 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/details', function(req, res) {
-  res.render('details');
+router.get('/main', function(req, res) {
+  res.render('singlepage');
 });
 
-router.get('/crime', function(req, res) {
-  res.render('crime');
-});
+// router.get('/crime', function(req, res) {
+//   res.render('crime');
+// });
 
-router.get('/realestate', function(req, res) {
-  res.render('realestate');
-});
+// router.get('/realestate', function(req, res) {
+//   res.render('realestate');
+// });
 
-router.get('/schools', function(req, res) {
-  res.render('schools');
-});
+// router.get('/schools', function(req, res) {
+//   res.render('schools');
+// });
 
-router.get('/citydetails', function(req, res) {
-  res.render('citydetails');
-});
+// router.get('/citydetails', function(req, res) {
+//   res.render('citydetails');
+// });
 
 module.exports = router;
